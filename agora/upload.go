d549f283bc931d229f22387833123a861454b56a
martin <martin.buehrer@gyrotools.com>
2024-12-13 13:06:02 +0100

    improvments in progress

--- agora/upload.go
+++ agora/upload.go
@@ -1,9 +1,7 @@
 package agora
 
 import (
-	"crypto/sha1"
-	"encoding/hex"
-	"io"
+	"errors"
 	"os"
 	"path/filepath"
 	"sync"
@@ -21,6 +19,23 @@ type CLIProgressHandler struct {
 	tracker map[string]*progress.Tracker
 }
 
+func (c *CLIProgressHandler) HandleImportProgress(progressChan <-chan agoraModels.UploadProgress, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for prog := range progressChan {
+		if prog.Type == agoraModels.TypeImportProgress {
+			if importProgress, ok := prog.Data.(int); ok {
+				c.tracker["import"].SetValue(int64(importProgress))
+			}
+		}
+	}
+	for _, t := range c.tracker {
+		if !t.IsDone() {
+			t.MarkAsDone()
+		}
+	}
+	c.Cleanup()
+}
+
 func (c *CLIProgressHandler) HandleProgress(progressChan <-chan agoraModels.UploadProgress, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for prog := range progressChan {
@@ -31,27 +46,41 @@ func (c *CLIProgressHandler) HandleProgress(progressChan <-chan agoraModels.Uplo
 		if prog.Type == agoraModels.TypeFileUploadStarted {
 			if fileData, ok := prog.Data.(agoraModels.UploadFile); ok {
 				if _, ok := c.tracker[fileData.SourcePath]; !ok {
-					t := &progress.Tracker{Message: fileData.TargetPath, Total: 100, Units: progress.UnitsDefault, RemoveOnCompletion: false}
+					t := &progress.Tracker{Message: filepath.Base(fileData.TargetPath), Total: fileData.Size, Units: progress.UnitsBytes, RemoveOnCompletion: true}
 					c.tracker[fileData.SourcePath] = t
 					c.pw.AppendTracker(t)
-					c.pw.SetPinnedMessages("Hello World")
 				}
 			}
 		}
 		if prog.Type == agoraModels.TypeFileProgress {
 			if fileProgressData, ok := prog.Data.(agoraModels.UploadProgressTransferData); ok {
 				if _, ok := c.tracker[fileProgressData.File.SourcePath]; !ok {
-					t := &progress.Tracker{Message: fileProgressData.File.TargetPath, Total: 100, Units: progress.UnitsDefault, RemoveOnCompletion: false}
+					t := &progress.Tracker{Message: filepath.Base(fileProgressData.File.TargetPath), Total: fileProgressData.File.Size, Units: progress.UnitsBytes, RemoveOnCompletion: true}
 					c.tracker[fileProgressData.File.SourcePath] = t
 					c.pw.AppendTracker(t)
 				}
-				pct := int64(float64(fileProgressData.BytesTransfered) / float64(fileProgressData.TotalSize) * 100)
-				c.tracker[fileProgressData.File.SourcePath].SetValue(pct)
+				c.tracker[fileProgressData.File.SourcePath].SetValue(fileProgressData.BytesTransfered)
+				c.tracker["total"].Increment(fileProgressData.BytesIncrement)
 			}
 		}
 		if prog.Type == agoraModels.TypeFileUploadCompleted {
 			if fileProgressData, ok := prog.Data.(agoraModels.UploadFile); ok {
 				c.tracker[fileProgressData.SourcePath].MarkAsDone()
+			}
+		}
+		if prog.Type == agoraModels.TypeImportProgress {
+			if importProgressData, ok := prog.Data.(agoraModels.ImportProgress); ok {
+				if _, ok := c.tracker["import"]; !ok {
+					t := &progress.Tracker{Message: "Import Progress", Total: 100, Units: progress.UnitsDefault, RemoveOnCompletion: false}
+					c.tracker["import"] = t
+					c.pw.AppendTracker(t)
+				}
+				c.tracker["import"].SetValue(int64(importProgressData.Progress))
+			}
+		}
+		if prog.Type == agoraModels.TypeUploadError {
+			if fileProgressData, ok := prog.Data.(agoraModels.UploadFile); ok {
+				c.tracker[fileProgressData.SourcePath].MarkAsErrored()
 			}
 		}
 	}
@@ -60,27 +89,24 @@ func (c *CLIProgressHandler) HandleProgress(progressChan <-chan agoraModels.Uplo
 			t.MarkAsDone()
 		}
 	}
-	// Add a small delay to allow the progress writer to update the display
-	time.Sleep(100 * time.Millisecond)
-
-	c.pw.Stop()
+	c.Cleanup()
 }
 
 func (c *CLIProgressHandler) Cleanup() {
-	c.pw.Stop()
 	time.Sleep(time.Millisecond * 100)
+	c.pw.Stop()
 }
 
-func NewCLIProgressHandler(nrParallelUploads int) *CLIProgressHandler {
+func NewCLIProgressHandler(nrExpectedTrackers int) *CLIProgressHandler {
 	var (
 		flagAutoStop           = false
 		flagHideETA            = true
 		flagHideETAOverall     = true
-		flagHideOverallTracker = false
+		flagHideOverallTracker = true
 		flagHidePercentage     = false
 		flagHideTime           = true
 		flagHideValue          = false
-		flagShowSpeed          = false
+		flagShowSpeed          = true
 		flagShowSpeedOverall   = false
 		flagShowPinned         = false
 	)
@@ -89,8 +115,8 @@ func NewCLIProgressHandler(nrParallelUploads int) *CLIProgressHandler {
 	pw.SetAutoStop(flagAutoStop)
 	pw.SetTrackerLength(40)
 	pw.SetMessageLength(30)
-	pw.SetNumTrackersExpected(nrParallelUploads)
-	pw.SetSortBy(progress.SortByPercentDsc)
+	pw.SetNumTrackersExpected(nrExpectedTrackers)
+	pw.SetSortBy(progress.SortByNone)
 	pw.SetStyle(progress.StyleDefault)
 	pw.SetTrackerPosition(progress.PositionRight)
 	pw.SetUpdateFrequency(time.Millisecond * 100)
@@ -118,93 +144,71 @@ type DataFile struct {
 	Sha1 string `json:"sha1"`
 }
 
-type ImportResult struct {
-	DataFiles []DataFile `json:"datafiles"`
-}
-
-func sha1Hash(file string) (string, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return "", err
+func printReport(result *agoraModels.ImportResult) {
+	logrus.Info("\nImport Result:")
+	logrus.Info("--------------")
+	logrus.Infof("Total Files : %d", result.NrFiles)
+	logrus.Infof("Uploaded    : %d", result.NrUploaded)
+	if result.NrUploadFailed > 0 || result.NrUploadHashFailed > 0 {
+		logrus.Infof("\033[31mFailed      : %d\033[0m", result.NrUploadFailed+result.NrUploadHashFailed)
+	} else {
+		logrus.Infof("Failed      : %d", result.NrUploadFailed+result.NrUploadHashFailed)
 	}
-	defer f.Close()
-
-	h := sha1.New()
-	if _, err := io.Copy(h, f); err != nil {
-		return "", err
+	if result.Datafiles != nil {
+		logrus.Infof("Imported    : %d", result.NrImported)
+		logrus.Infof("Existed     : %d", result.NrExisted)
+		if result.NrIgnored > 0 {
+			logrus.Infof("\033[33;1mIgnored     : %d\033[0m", result.NrIgnored)
+		} else {
+			logrus.Infof("Ignored     : %d", result.NrIgnored)
+		}
 	}
 
-	hash := h.Sum(nil)
-	return hex.EncodeToString(hash[:]), nil
+	if result.NrUploadFailed > 0 {
+		logrus.Info(" ")
+		logrus.Errorf("%d files failed to upload:", result.NrUploadFailed)
+		for _, file := range result.UploadFailed {
+			logrus.Errorf("  - %s", file)
+		}
+	}
+	if result.NrUploadHashFailed > 0 {
+		logrus.Info(" ")
+		logrus.Errorf("%d files were uploaded but the hash verification failed:", result.NrUploadHashFailed)
+		for _, file := range result.UploadHashFailed {
+			logrus.Errorf("  - %s", file)
+		}
+	}
+	if result.NrExisted > 0 {
+		logrus.Info(" ")
+		logrus.Warningf("%d files were not imported because they already existed in Agora:", result.NrExisted)
+		for _, file := range result.Existed {
+			logrus.Warningf("  - %s", file)
+		}
+	}
+	if result.NrIgnored > 0 {
+		logrus.Info(" ")
+		logrus.Warningf("%d files were ignored during the import. This is most likely not an error", result.NrIgnored)
+		for _, file := range result.Ignored {
+			logrus.Warningf("  - %s", file)
+		}
+	}
+	if result.NrHashFailed > 0 {
+		logrus.Info(" ")
+		logrus.Errorf("%d files were imported but the hash verification failed:", result.NrHashFailed)
+		for _, file := range result.HashFailed {
+			logrus.Errorf("  - %s", file)
+		}
+	}
+	logrus.Info(" ")
+	if result.NrFiles == result.NrUploaded {
+		logrus.Info("\033[32mAll files were uploaded successfully!\033[0m")
+	}
+	if result.Datafiles != nil {
+		if result.NrFiles == result.NrImported+result.NrExisted {
+			logrus.Info("\033[32mAll files were imported successfully!\033[0m")
+		}
+	}
 }
-
-// func update_import_state(files []agoraModels.UploadFile, agora_url string, importPackageId int, api_key string) (bool, error) {
-// 	logrus.Info("\nChecking Imports:")
-// 	logrus.Info("-----------------")
-// 	request_url := join_url(agora_url, "/api/v1/import/")
-// 	request_url = join_url(request_url, fmt.Sprintf("/%d/", importPackageId))
-// 	request_url = join_url(request_url, "/result/")
-
-// 	response, err := GetRequest(request_url, api_key, "", "")
-// 	if err != nil {
-// 		return false, err
-// 	}
-// 	defer response.Body.Close()
-
-// 	if response.StatusCode != http.StatusOK {
-// 		err_status := fmt.Errorf("could not get the import result. http status = %d", response.StatusCode)
-// 		return false, err_status
-// 	}
-// 	body, err := ioutil.ReadAll(response.Body)
-// 	if err != nil {
-// 		return false, err
-// 	}
-
-// 	var data []ImportResult
-// 	err = json.Unmarshal(body, &data)
-// 	if err != nil {
-// 		return false, err
-// 	}
-
-// 	datafiles := []DataFile{}
-// 	if len(data) > 0 && len(data[0].DataFiles) == 0 {
-// 		return true, nil
-// 	}
-// 	for _, entry := range data {
-// 		datafiles = append(datafiles, entry.DataFiles...)
-// 	}
-// 	uniqueDataFiles := make(map[int]DataFile)
-// 	for _, datafile := range datafiles {
-// 		uniqueDataFiles[datafile.ID] = datafile
-// 	}
-// 	datafiles = []DataFile{}
-// 	for _, datafile := range uniqueDataFiles {
-// 		datafiles = append(datafiles, datafile)
-// 	}
-// 	for _, datafile := range datafiles {
-// 		for _, file := range files {
-// 			if filepath.Base(file.TargetPath) == filepath.Base(datafile.Name) && !file.Imported {
-// 				localSha1, err := sha1Hash(file.SourcePath)
-// 				status := "FAILED:  "
-// 				//statusColor := color.New(color.FgRed).SprintFunc()
-// 				if err != nil {
-// 					status = "UNKNOWN:"
-// 					//statusColor = color.New(color.FgYellow).SprintFunc()
-// 				}
-// 				if localSha1 == datafile.Sha1 {
-// 					file.Imported = true
-// 					status = "IMPORTED:"
-// 					//statusColor = color.New(color.FgGreen).SprintFunc()
-// 				} else {
-// 					fmt.Println(localSha1)
-// 				}
-// 				logrus.Infof("%s %s\t", status, file.SourcePath)
-// 				break
-// 			}
-// 		}
-// 	}
-// 	return true, nil
-// }
 
 func Upload(agora_url string, api_key string, file_or_dir string, target_folder_id int, extract_zip bool, json_import_file string, wait bool, timeout int, verify bool, fake bool) error {
 	if extract_zip {
@@ -214,16 +218,23 @@ func Upload(agora_url string, api_key string, file_or_dir string, target_folder_
 				logrus.Warningf("\"--extract-zip\" has no effect when uploading a directory and will be ignored")
 			} else if filepath.Ext(file_or_dir) != ".zip" {
 				logrus.Warningf("no zip file found. \"--extract-zip\" will be ignored")
+			} else if verify {
+				// the import cannot be verified if a zip file is uploaded and extracted
+				verify = false
 			}
 		}
 	}
-
-	input_files := []string{file_or_dir}
 	logrus.Debugf("Starting upload of %s to %s", file_or_dir, agora_url)
 
 	agora, err := agoraConn.Create(agora_url, api_key, false)
 	if err != nil {
 		return err
+	}
+
+	// check if the target folder exists
+	_, err = agora.GetFolder(target_folder_id)
+	if err != nil {
+		return errors.New("the Agora target folder does not exist")
 	}
 
 	importPackage, err := agora.NewImportPackage()
@@ -239,12 +250,17 @@ func Upload(agora_url string, api_key string, file_or_dir string, target_folder_
 		for prog := range agoraProgressChan {
 			if prog.Type == agoraModels.TypeUploadInitialized {
 				if initData, ok := prog.Data.(agoraModels.UploadProgressInitData); ok {
-					logrus.Infof("Found %d files larger than %dMB which will be uploaded directly", initData.FilesToUpload, agoraModels.UPLOAD_CHUCK_SIZE/1024/1024)
-					logrus.Infof("Found %d files which will be zipped and uploaded", initData.FilesToZip)
+					logrus.Infof("%d files will be uploaded directly", initData.FilesToUpload)
+					logrus.Infof("%d files which will be zipped and uploaded", initData.FilesToZip)
 					logrus.Info("\nUploading Data:")
 					logrus.Info("-----------------")
 
-					progressHandler := NewCLIProgressHandler(initData.NrFilesToUpload)
+					// add the total progress tracker
+					progressHandler := NewCLIProgressHandler(agoraModels.PARALLEL_UPLOADS + 1)
+					t := &progress.Tracker{Message: "Total", Total: initData.TotalSize, Units: progress.UnitsBytes, RemoveOnCompletion: false}
+					progressHandler.tracker["total"] = t
+					progressHandler.pw.AppendTracker(t)
+
 					go progressHandler.HandleProgress(agoraProgressChan, &wgUpload)
 					return
 				}
@@ -252,15 +268,11 @@ func Upload(agora_url string, api_key string, file_or_dir string, target_folder_
 		}
 	}()
 
-	uploadFiles := make([]agoraModels.UploadFile, 0)
-	for _, file := range input_files {
-		uploadFile, err := agoraModels.NewUploadFile(file, nil)
-		if err != nil {
-			return err
-		}
-		uploadFiles = append(uploadFiles, uploadFile)
+	uploadFile, err := agoraModels.NewUploadFile(file_or_dir, nil)
+	if err != nil {
+		return err
 	}
-	err = importPackage.Upload(uploadFiles, agoraProgressChan)
+	err = importPackage.Upload([]agoraModels.UploadFile{uploadFile}, agoraProgressChan)
 	if err != nil {
 		return err
 	}
@@ -272,10 +284,39 @@ func Upload(agora_url string, api_key string, file_or_dir string, target_folder_
 	logrus.Info("Finalizing Uploads")
 	var wgComplete sync.WaitGroup
 	wgComplete.Add(1)
-	err = importPackage.Complete(target_folder_id, json_import_file, false, &wgComplete)
+	err = importPackage.Complete(target_folder_id, json_import_file, extract_zip, &wgComplete)
 	if err != nil {
 		return err
 	}
 	wgComplete.Wait()
+	if verify {
+		logrus.Info("\nWaiting for the Imports to finish...")
+
+		importProgressChan := make(chan agoraModels.UploadProgress)
+		// this wait group waits until all import progress messages are printed
+		var wgImportProgress sync.WaitGroup
+		wgImportProgress.Add(1)
+
+		// create an own progress handler for the import
+		importProgressHandler := NewCLIProgressHandler(1)
+		t := &progress.Tracker{Message: "Import", Total: 100, Units: progress.UnitsDefault, RemoveOnCompletion: false}
+		importProgressHandler.tracker["import"] = t
+		importProgressHandler.pw.AppendTracker(t)
+		go importProgressHandler.HandleImportProgress(importProgressChan, &wgImportProgress)
+
+		// query the progress of the import
+		err = importPackage.WaitForImport(time.Duration(30)*time.Minute, importProgressChan)
+		if err != nil {
+			return err
+		}
+		// close the progress channel and wait for the progress messages to be printed
+		close(importProgressChan)
+		wgImportProgress.Wait()
+	}
+	result, err := importPackage.Result()
+	if err != nil {
+		return err
+	}
+	printReport(result)
 	return nil
 }
